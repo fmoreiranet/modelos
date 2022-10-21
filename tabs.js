7642dd5162b6e78c13a6f1e02eba733950c9b47b
fMoreira <fabianomoreira.net@gmail.com>
2022-10-20 21:45:22 -0300

    tabs

---
+++ tabs.js
@@ -0,0 +1,12 @@
+(function () {
+    var tabs = document.querySelectorAll(".tab")
+    tabs.addEventListener("click", function (e) {
+        selectTab(e, this);
+    });
+
+})();
+
+function selectTab(event, obj) {
+    console.log(event);
+    console.log(obj);
+}
